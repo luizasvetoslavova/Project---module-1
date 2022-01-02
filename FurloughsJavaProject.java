7fc34d5321716baf4b29bbf6c9e7cc6afd3f0111
luizasvetoslavova <luizasvetoslavova16@gmail.com>
2022-01-02 22:00:05 +0200

    02/01

--- FurloughsJavaProject.java
+++ FurloughsJavaProject.java
@@ -1,14 +1,14 @@
 package com.company;
-import jdk.internal.access.JavaNetHttpCookieAccess;
-import java.time.LocalDate;
+import java.text.ParseException;
 import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class FurloughsJavaProject {
 
     public static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         setOptions();
         String userChoice = scanner.nextLine();
         processUserChoice(userChoice);
@@ -28,10 +28,8 @@ public class FurloughsJavaProject {
                 Вашият избор:\s""");
     }
 
-    public static void processUserChoice(String userChoice) {
-
-        while (!userChoice.equals("1") && !userChoice.equals("2") &&
-                !userChoice.equals("3") && !userChoice.equals("4") && !userChoice.equals("5")) {
+    public static void processUserChoice(String userChoice) throws ParseException {
+        while (!isUserChoiceInLimit(userChoice)) {
             printIllegalInputMessage();
             userChoice = scanner.nextLine();
         }
@@ -45,58 +43,85 @@ public class FurloughsJavaProject {
         }
     }
 
-    public static void stateAFurlough() {
+    public static boolean isUserChoiceInLimit(String userChoice) {
+        return userChoice.equals("1") || userChoice.equals("2") ||
+                userChoice.equals("3") || userChoice.equals("4") || userChoice.equals("5");
+    }
+
+    public static void stateAFurlough() throws ParseException {
         setUserName();
         setUserEmail();
         setUserID();
-        setFurloughPeriod();
+        setFurloughPeriod(setFurloughStartDate(), setFurloughEndDate());
         setFurloughType();
     }
 
     public static void setFurloughType() {
+        System.out.print("""
+                Изберете тип отпуска (1-2):\s
+                1. Платена\s
+                2. Неплатена\s
+                Вашият избор:\s""");
+
+        String furloughType = scanner.nextLine();
+
+        while (!isFurloughTypeInLimit(furloughType)) {
+            printIllegalInputMessage();
+            furloughType = scanner.nextLine();
+        }
     }
 
-    public static void setFurloughPeriod() {
-        setFurloughStartDate();
-        setFurloughEndDate();
+    public static boolean isFurloughTypeInLimit(String furloughType) {
+        return furloughType.equals("1") || furloughType.equals("2");
     }
 
-    private static void setFurloughStartDate() {
-        System.out.println("Моля, въведете начало на вашата отпуска във формат ДД-ММ-ГГ: ");
-        String inputDate = scanner.nextLine();
+    public static void setFurloughPeriod(String startDate, String endDate) throws ParseException {
+        Date start = new SimpleDateFormat("dd-MM-yyyy").parse(startDate);
+        Date end = new SimpleDateFormat("dd-MM-yyyy").parse(endDate);
 
-
-//        while () {
-//            printIllegalInputMessage();
-//        }
+        while (start.compareTo(end) >= 0) {
+            printIllegalInputMessage();
+            setFurloughStartDate();
+            setFurloughEndDate();
+        }
+        //TODO
     }
 
-    private static void setFurloughEndDate() {
-        System.out.println("Моля, въведете край на вашата отпуска във формат ДД-ММ-ГГ: ");
+    private static String setFurloughStartDate() {
+        System.out.print("Начало на вашата отпуска във формат ДД-ММ-ГГГГ: ");
+        String startDate = scanner.nextLine();
 
+        while (!isDateValid(startDate)) {
+            printIllegalInputMessage();
+            startDate = scanner.nextLine();
+        }
+        return startDate;
     }
 
-    public static boolean isEndDateInLimit(int day, int month, int year) {
-        LocalDate currentdate = LocalDate.now();
-        return !(day < currentdate.getDayOfMonth() ||
-                month < currentdate.getMonthValue() || year != currentdate.getYear());
+    private static String setFurloughEndDate() {
+        System.out.print("Край на вашата отпуска във формат ДД-ММ-ГГГГ: ");
+        String endDate = scanner.nextLine();
+
+        while (!isDateValid(endDate)) {
+            printIllegalInputMessage();
+            endDate = scanner.nextLine();
+        }
+        return endDate;
     }
 
-    private static boolean isStartDateInLimit(int day, int month, int year) {
-        LocalDate currentdate = LocalDate.now();
-        return !(day < currentdate.getDayOfMonth() ||
-                month < currentdate.getMonthValue() || year != currentdate.getYear());
+    public static boolean isDateValid(String input) {
+        return input.matches("^(((0[1-9]|[12][0-9]|30)[-]?(0[13-9]|1[012])|31[-]?(0[13578]|1[02])|(0[1-9]|1[0-9]|2[0-8])[-]?02)[-]?[0-9]{4}|29[-]?02[-]?([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00))$");
     }
 
     public static void printIllegalInputMessage() {
-        System.out.print("Невалиден вход! Опитайте отново: ");
+        System.out.println("Невалиден вход! Опитайте отново: ");
     }
 
     public static void setUserID() {
         System.out.print("Вашето ЕГН: ");
         String inputID = scanner.nextLine();
 
-        while (!isNumerous(inputID) || inputID.length() != 10) {
+        while (!isNumeric(inputID) || inputID.length() != 10) {
             printIllegalInputMessage();
             inputID = scanner.nextLine();
         }
@@ -106,12 +131,14 @@ public class FurloughsJavaProject {
         System.out.print("Вашият e-mail: ");
         String userEmail = scanner.nextLine();
 
-        // luizasvetoslavova16@gmail.com
-// TODO
-        while (userEmail.isBlank()) {
+        while (!isEmailValid(userEmail)) {
             printIllegalInputMessage();
             userEmail = scanner.nextLine();
         }
+    }
+
+    public static boolean isEmailValid(String input) {
+        return input.matches("^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$");
     }
 
     public static void setUserName() {
@@ -120,19 +147,24 @@ public class FurloughsJavaProject {
         System.out.print("Фамилия: ");
         String lastName = scanner.nextLine();
 
-        while (firstName.isBlank() || lastName.isBlank() ||
-                Character.isLowerCase(firstName.charAt(0)) || Character.isLowerCase(lastName.charAt(0)) ||
-                isNumerous(firstName) || isNumerous(lastName)) {
+        while (isUserNameIncorrect(firstName, lastName)) {
             printIllegalInputMessage();
             System.out.print("Име: ");
             firstName = scanner.nextLine();
             System.out.print("Фамилия: ");
             lastName = scanner.nextLine();
         }
+        //TODO
     }
 
-    public static boolean isNumerous(String input) {
-        return input.matches("[+-]?\\d*(\\.\\d+)?");
+    public static boolean isUserNameIncorrect(String firstName, String lastName) {
+        return firstName.isBlank() || lastName.isBlank() ||
+                Character.isLowerCase(firstName.charAt(0)) || Character.isLowerCase(lastName.charAt(0)) ||
+                isNumeric(firstName) || isNumeric(lastName);
+    }
+
+    public static boolean isNumeric(String input) {
+        return input.matches("^[-+]?\\d*\\.?\\d+$");
     }
 
     private static void exitFurloughSystem() {
@@ -146,20 +178,4 @@ public class FurloughsJavaProject {
 
     private static void seeAllFurloughs() {
     }
-
 }
-//inputID.matches("[+-]?\\d*(\\.\\d+)?")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
